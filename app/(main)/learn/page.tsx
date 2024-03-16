14066e3ba91205dc9409e5b26e4a72ca82381599
Davide Filosa <davideclaypool@gmail.com!>
2024-03-16 07:39:32 +0100

    created db connectio

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,25 @@
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
 import React from "react";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
-  return <div>Lern</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse="test title"
+          hearts={5}
+          points={5}
+          hasActiveSubscription={true}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="header title" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
